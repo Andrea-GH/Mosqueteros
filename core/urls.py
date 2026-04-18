c6ea96f46ad174a5275e7ab9ba6d46d1ec206bd1
angel-ob001 <cc4987073@gmail.com>
2026-04-17 19:05:22 -0600

    Se agrego el sistema de login profesional

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,19 @@
+# Ubicación: core/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
+    
+    # 1. Login como página de inicio
+    path('', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    
+    # 2. Rutas de autenticación de Django
+    path('accounts/', include('django.contrib.auth.urls')),
+    
+    # 3. Tus aplicaciones (Sin namespaces para que los botones funcionen directo)
+    path('dashboard/', include('dashboard.urls')),
     path('catalogos/', include('catalogos.urls')),
-    path('ventas/', include('ventas.urls')),
-]
+    path('ventas/', include('ventas.urls')), 
+    ]
